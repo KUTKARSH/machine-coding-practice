081acc7ba0b74ee10f05d78f922edbbced400e01
Kumar Utkarsh Acko <kumar.utkarsh@acko.tech>
2025-05-08 19:52:19 +0530

    Observer pattern implemented !

---
+++ src/main/java/designpatterns/NotificationSystem.java
@@ -0,0 +1,74 @@
+package designpatterns;
+
+import lombok.AllArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A demonstration of Observer Pattern
+ */
+
+
+interface Observer {
+    void notify(String msg);
+}
+
+@AllArgsConstructor
+class SmsSubscriber implements Observer {
+
+    private String name;
+
+    @Override
+    public void notify(String msg) {
+        System.out.println("User: " + name + " received sms: " + msg);
+    }
+}
+
+@AllArgsConstructor
+class EmailSubscriber implements Observer {
+
+    private String name;
+
+    @Override
+    public void notify(String msg) {
+        System.out.println("User: " + name + " received email: " + msg);
+    }
+}
+
+class NewsAgency {
+    private final List<Observer> observers;
+
+    NewsAgency() {
+        this.observers = new ArrayList<>();
+    }
+
+    public void subscribe(Observer o) {
+        observers.add(o);
+    }
+
+    public void unsubscribe(Observer o) {
+        observers.remove(o);
+    }
+
+    public void publish(String msg) {
+        observers.forEach(observer -> observer.notify(msg));
+    }
+
+}
+
+public class NotificationSystem {
+
+    public static void main(String[] args) {
+        NewsAgency agency = new NewsAgency();
+
+        Observer emailSubscriber = new EmailSubscriber("Alice");
+        Observer smsSubscriber = new SmsSubscriber("Bob");
+
+        agency.subscribe(emailSubscriber);
+        agency.subscribe(smsSubscriber);
+
+        agency.publish("Observer Pattern Implemented!");
+    }
+
+}
