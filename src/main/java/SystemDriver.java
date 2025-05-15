4c96f9cbff5627d29eb0f5c61ffd2d71f439f62b
Kumar Utkarsh Acko <kumar.utkarsh@acko.tech>
2025-05-16 02:25:05 +0530

    Removed dummy driver

--- src/main/java/SystemDriver.java
+++
@@ -1,7 +0,0 @@
-public class SystemDriver {
-
-    public static void main(String[] args) {
-        app_version_management.Driver driver = new app_version_management.Driver();
-        driver.run();
-    }
-}
